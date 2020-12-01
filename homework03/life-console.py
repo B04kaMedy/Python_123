637995ea90e486800dfb969dcd3242c863ea05e9
312565 Михаил Кулешов <b04kamedy@gmail.com>
2020-12-01 15:34:15 +0300

    Border is drawing succesfully!

--- homework03/life-console.py
+++ homework03/life-console.py
@@ -11,7 +11,21 @@ class Console(UI):
 
     def draw_borders(self, screen) -> None:
         """ Отобразить рамку. """
-        pass
+        horisontal_line = "+"
+        for i in range(self.life.cols):
+            horisontal_line += "-"
+        horisontal_line += "+"
+
+        vertical_line = "|"
+        for i in range(self.life.cols):
+            vertical_line += " "
+        vertical_line += "|"
+
+        screen.addstr(0, 0, horisontal_line)
+        for j in range(1, self.life.rows+1):
+            screen.addstr(j, 0, vertical_line)
+        screen.addstr(j, 0, horisontal_line)
+        screen.refresh()
 
     def draw_grid(self, screen) -> None:
         """ Отобразить состояние клеток. """
@@ -20,4 +34,28 @@ class Console(UI):
     def run(self) -> None:
         screen = curses.initscr()
         # PUT YOUR CODE HERE
+        self.draw_borders(screen)
+        curses.napms(3000)
+        # self.draw_grid(screen)
+
         curses.endwin()
+
+life = GameOfLife((24, 80))
+ui = Console(life)
+ui.run()
+
+
+
+# import curses
+# screen = curses.initscr()
+# # Update the buffer, adding text at different locations
+# screen.addstr(0, 0, "This string gets printed at position (0, 0)")
+# screen.addstr(3, 1, "Try Russian text: Привет")  # Python 3 required for unicode
+# screen.addstr(4, 4, "X")
+# screen.addch(5, 5, "Y")
+#
+# # Changes go in to the screen buffer and only get
+# # displayed after calling `refresh()` to update
+# screen.refresh()
+# curses.napms(3000)
+# curses.endwin()
