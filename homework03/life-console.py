9a661c5a8f364ca7b0ff168f9e32b903cb262e1a
312565 Михаил Кулешов <b04kamedy@gmail.com>
2020-12-02 01:45:59 +0300

    Works basically

--- homework03/life-console.py
+++ homework03/life-console.py
@@ -24,38 +24,40 @@ class Console(UI):
         screen.addstr(0, 0, horisontal_line)
         for j in range(1, self.life.rows+1):
             screen.addstr(j, 0, vertical_line)
-        screen.addstr(j, 0, horisontal_line)
+        screen.addstr(self.life.rows+1, 0, horisontal_line)
+
+        curses.curs_set(0)
+
         screen.refresh()
 
     def draw_grid(self, screen) -> None:
         """ Отобразить состояние клеток. """
-        pass
+        for i in range(self.life.rows):
+            edited_line = ''
+            edited_line += '|'
+            for j in range(self.life.cols):
+                if self.life.curr_generation[i][j] == 1:
+                    edited_line += '*'
+                else:
+                    edited_line += ' '
+            edited_line += '|'
+            screen.addstr(i+1, 0, edited_line)
+        screen.refresh()
 
     def run(self) -> None:
         screen = curses.initscr()
         # PUT YOUR CODE HERE
+        # my_window = curses.newwin(15, 20, 0, 0)
         self.draw_borders(screen)
-        curses.napms(3000)
-        # self.draw_grid(screen)
-
+        while life.is_changing and not life.is_max_generations_exceed:
+            life.step()
+            self.draw_grid(screen)
+            curses.napms(300)
+        screen.addstr(self.life.rows+2, 0, "Нажмите любую клавишу, чтобы выйти.")
+        c = screen.getch()
         curses.endwin()
+        # print("You pressed %s which is keycode %d." % (chr(c), c))
 
-life = GameOfLife((24, 80))
+life = GameOfLife((24, 80), max_generations=10)
 ui = Console(life)
 ui.run()
-
-
-
-# import curses
-# screen = curses.initscr()
-# # Update the buffer, adding text at different locations
-# screen.addstr(0, 0, "This string gets printed at position (0, 0)")
-# screen.addstr(3, 1, "Try Russian text: Привет")  # Python 3 required for unicode
-# screen.addstr(4, 4, "X")
-# screen.addch(5, 5, "Y")
-#
-# # Changes go in to the screen buffer and only get
-# # displayed after calling `refresh()` to update
-# screen.refresh()
-# curses.napms(3000)
-# curses.endwin()
